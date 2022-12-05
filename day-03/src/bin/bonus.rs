b6bf3722bbd65aa29b7087910c79f79ade58a466
DasLixou <einlixou@gmail.com>
2022-12-05 15:24:20 +0100

    Day 03 - Bonus

---
+++ day-03/src/bin/bonus.rs
@@ -0,0 +1,3 @@
+pub fn main() {
+    day_03::bonus()
+}
--- day-03/src/lib.rs
+++ day-03/src/lib.rs
@@ -23,3 +23,29 @@ pub fn normal() {
         .sum();
     println!("Result: {result}");
 }
+
+pub fn bonus() {
+    let input = include_str!("input.txt");
+
+    let scores: HashMap<char, usize> = ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|(i, c)| (c, i + 1))
+        .collect::<HashMap<char, usize>>();
+
+    let result: usize = input
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|lines| {
+            let (first, second, third) = (lines[0], lines[1], lines[2]); // is there a cleaner way?
+            let item = first
+                .chars()
+                .find(|i| second.contains(*i) && third.contains(*i))
+                .expect("No item type that could be found in both compartments");
+            scores.get(&item).unwrap()
+        })
+        .sum();
+
+    println!("Result: {result}");
+}
