d7ab74ade60528c846094b9bfb8442a31c9d7bbf
DasLixou <einlixou@gmail.com>
2022-12-06 14:48:41 +0100

    Day 04 - Bonus

--- day-04/src/lib.rs
+++ day-04/src/lib.rs
@@ -24,5 +24,34 @@ pub fn normal() {
             inside(first, second) || inside(second, first)
         })
         .count();
-    println!("{result}");
+    println!("Result: {result}");
+}
+
+pub fn bonus() {
+    let input = include_str!("input.txt");
+    let result = input
+        .lines()
+        .filter(|line| {
+            let pairs = line
+                .split(',')
+                .map(|range_str| {
+                    range_str
+                        .split('-')
+                        .map(|point| point.parse::<u32>().expect("Couldn't convert to number"))
+                        .collect::<Vec<u32>>()
+                })
+                .collect::<Vec<Vec<u32>>>();
+
+            #[inline]
+            const fn overlap(first: (u32, u32), second: (u32, u32)) -> bool {
+                first.1 >= second.0 && second.1 >= first.0
+            }
+
+            let first = (pairs[0][0], pairs[0][1]);
+            let second = (pairs[1][0], pairs[1][1]);
+
+            overlap(first, second) || overlap(second, first)
+        })
+        .count();
+    println!("Result: {result}");
 }
