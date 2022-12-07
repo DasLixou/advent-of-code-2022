63d567ba20258bc7f45f7859592185c25e2f291e
DasLixou <einlixou@gmail.com>
2022-12-07 16:52:30 +0100

    Day 06 - Bonus

--- day-06/src/lib.rs
+++ day-06/src/lib.rs
@@ -1,9 +1,10 @@
 pub fn normal() {
     let input = include_str!("input.txt");
+    const LOOK_AHEAD: usize = 4;
     let result = input
         .chars()
         .collect::<Vec<char>>()
-        .windows(4)
+        .windows(LOOK_AHEAD)
         .enumerate()
         .find_map(|(i, seq)| {
             if seq
@@ -13,7 +14,31 @@ pub fn normal() {
             {
                 None
             } else {
-                Some(i + 4)
+                Some(i + LOOK_AHEAD)
+            }
+        })
+        .expect("No marker found :c");
+
+    println!("Result: {result}");
+}
+
+pub fn bonus() {
+    let input = include_str!("input.txt");
+    const LOOK_AHEAD: usize = 14;
+    let result = input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(LOOK_AHEAD)
+        .enumerate()
+        .find_map(|(i, seq)| {
+            if seq
+                .iter()
+                .enumerate()
+                .any(|(j, c)| seq[j + 1..].contains(c))
+            {
+                None
+            } else {
+                Some(i + LOOK_AHEAD)
             }
         })
         .expect("No marker found :c");
